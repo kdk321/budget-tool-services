b0ded324dc04174a33f0c6d115cef314fc584a47
Kent Koski <kent.koski@aa.com>
2019-12-16 17:12:39 -0700

    Initial project creation

---
+++ src/main/java/com/budgettool/budgettoolservices/BudgetToolServicesApplication.java
@@ -0,0 +1,13 @@
+package com.budgettool.budgettoolservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetToolServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetToolServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/budgettool/budgettoolservices/BudgetToolServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgettool.budgettoolservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetToolServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
